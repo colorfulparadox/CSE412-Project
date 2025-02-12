08c0a38fde85ec07179d66909fb7a3a03b0b7c1a
Preston <101310062+colorfulparadox@users.noreply.github.com>
2025-02-11 20:29:12 -0700

    db setup

--- database/create_users.py
+++ database/create_users.py
@@ -1,1 +1,30 @@
-print("hello world")
+import psycopg2
+import bcrypt
+
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="password",
+    host="localhost",
+    port="5430"
+)
+
+try:
+    cur = conn.cursor()
+    
+    username = "admin"
+    password = "password".encode('utf-8')
+    hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    email = "heewook.lee@asu.edu"
+
+    cur.execute("""
+        INSERT INTO UserData (username, password, email)
+        VALUES (%s, %s, %s)
+        ON CONFLICT (username) DO NOTHING;
+    """, (username, hash_password, email))
+    conn.commit()
+    cur.close()
+except Exception as e:
+    conn.rollback()
+    print(f"Adding Users to DB Error: {e}")
+    exit(1)
--- database/setup.sh
+++ database/setup.sh
@@ -7,8 +7,11 @@ mkdir postgres_data
 echo " ### Building and Running Docker ###"
 docker-compose up --detach
 
-# bad code smell, however I want to make sure
-# docker is running
-echo "Adding in init data to db"
+
 sleep 1
+
+mkdir .venv
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r python_requirements.txt
 python3 create_users.py
