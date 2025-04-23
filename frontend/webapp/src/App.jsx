724dba02669fc3f36212463ffdf0a168f1bc6a46
Preston <101310062+colorfulparadox@users.noreply.github.com>
2025-04-23 14:20:00 -0700

    web page route

--- frontend/webapp/src/App.jsx
+++ frontend/webapp/src/App.jsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import LandingPage from './pages/landing_page.jsx';
+import Login from './pages/login.jsx';
+import Page404 from './pages/404.jsx';
+import UserProfile from './pages/user_profile.jsx';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App() {
+  const path = window.location.pathname
+  if (path === "/") {
+    return <LandingPage/>
+  }
+  else if ((path.startsWith("/login"))) {
+    return <Login/>
+  }
+  else if ((path.startsWith("/UserProfile"))) {
+    return <UserProfile/>
+  }
+  else {
+    return <Page404/>
+  }
 }
 
 export default App
---
+++ frontend/webapp/src/pages/404.jsx
@@ -0,0 +1,3 @@
+export default function Page404() {
+    return <h1>You dumb bitch! Its a 404 Error!</h1>;
+}
