462633570eca762037ee16be830ff5714d63cdd4
Skyler Riske <sriske2@users.noreply.github.com>
2025-04-23 14:13:11 -0700

    basic react-boostrap navbar

---
+++ frontend/webapp/src/components/navbar.jsx
@@ -0,0 +1,36 @@
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import { useEffect, useState } from 'react';
+
+function Logout(event, navigate){
+  event.preventDefault();
+  navigate('/');
+}
+
+export default function NavBar() {
+
+  const [user, setAdminData] = useState(null);
+
+    const onSubmit = (event) => {
+        Logout(event, navigate)
+    }
+
+    return (
+        <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="Home">Title</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Nav className="me-auto">
+            <Nav.Link href="/">navlink</Nav.Link>
+            <Nav.Link href="/">navlink</Nav.Link>
+            <Nav.Link href="/">navlink</Nav.Link>
+            <Nav.Link href="/">navlink</Nav.Link>
+          </Nav>
+          <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+          <Button className="" variant="outline-danger" type="submit" onClick={onSubmit}>Logout</Button>
+        </Container>
+      </Navbar>
+    )
+}
