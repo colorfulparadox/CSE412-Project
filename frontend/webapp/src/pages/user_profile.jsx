1f9db3562b35ec3cf1eb7cb54db2c6fa4546589a
Skyler Riske <sriske2@users.noreply.github.com>
2025-04-23 18:05:05 -0700

    barebones user profile page

--- frontend/webapp/src/pages/user_profile.jsx
+++ frontend/webapp/src/pages/user_profile.jsx
@@ -1,13 +1,125 @@
 import NavBar from '../components/navbar'; 
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { useEffect, useState, useRef } from 'react';
 
-export default function UserProfile() {
+// fields should populate on load
+
+export default function Profile() {
+    const [username, setUsername] = useState("");
+    const [name, setName] = useState("");
+    const [blurb, setBlurb] = useState("");
+    const password_reference = useRef("");
+  
+    // on load
+    useEffect(() => {
+        // setUsername(username);
+        // setName(name);
+        // setBlurb(blurb);
+      }, []);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (username === "" || name === "") {
+            return;
+        }
+
+        // update
+
+    }
+
+    const pwupdate_submit = (event) => {
+        event.preventDefault();
+
+        var pw = password_reference.current.value;
+    
+        if (pw === "") {
+            console.log("Don't submit an empty password");
+            return;
+        }
+
+    }
+
+    const handleChange = (event) => {
+        event.preventDefault();
+    
+        switch (event.target.name) {
+            case 'user':
+                setUsername(event.target.value);
+                userData[event.target.name] = event.target.value;
+                break;
+            case 'name':
+                setName(event.target.value);
+                userData[event.target.name] = event.target.value;
+                break;
+            case 'blurb':
+                setBlurb(event.target.value);
+                userData[event.target.name] = event.target.value;
+                break;
+        }
+    
+    }
+
     return (
         <>
-        <NavBar />
-        <Container style={{ padding: '20px' }}>
-            <h1>Hello User!</h1>
-        </Container>
+            <NavBar />
+            <Container>
+                <Row>
+                    <Col style={{ padding: '20px' }}>
+                        <h1>User Profile</h1>
+                        <Form.Group>
+                            <Form.Label htmlFor="inputUsername">Username</Form.Label>
+                            <Form.Control 
+                                type="username"
+                                name="username"
+                                value={username}
+                                className="mb-3"
+                                id="inputUsername"
+                                onChange={handleChange}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label htmlFor="inputName">Name</Form.Label>
+                            <Form.Control 
+                                type="name"
+                                name="name"
+                                value={name}
+                                className="mb-3"
+                                id="inputName"
+                                onChange={handleChange}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label htmlFor="inputBlurb">Blurb</Form.Label>
+                            <Form.Control 
+                                type="blurb"
+                                name="blurb"
+                                value={blurb}
+                                className="mb-3"
+                                id="inputBlurb"
+                                onChange={handleChange}/>
+                        </Form.Group>
+                        
+                        <Form.Label id="error-label" hidden>Error</Form.Label><br/>
+                        <Button variant="primary" type="submit" 
+                        className="mb-3"
+                        onClick={handleSubmit}
+                        >Update</Button>
+                        <br/>
+                        <Form.Group>
+                            <Form.Label htmlFor="pw_input">Set new password</Form.Label>
+                            <Form.Control 
+                                className="mb-3"
+                                ref={password_reference}
+                                name="password"
+                                type="password"
+                                id="pw_input"/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" 
+                            className="mb-3"
+                            onClick={pwupdate_submit}
+                            >Update Password</Button>
+                    </Col>
+                </Row>
+            </Container>
         </>
-    );
+    )
 }
